4383b63bc62d3dad5ac13bafa152c3741cd15629
Utkarshsteve <utkarshsteve7@gmail.com>
2021-05-21 14:52:48 +0530

    Dealing with ngModel

--- src/app/hero-detail/hero-detail.component.spec.ts
+++ src/app/hero-detail/hero-detail.component.spec.ts
@@ -1,27 +1,42 @@
-import { TestBed } from "@angular/core/testing"
+import { ComponentFixture, TestBed } from "@angular/core/testing"
 import { ActivatedRoute } from "@angular/router";
 import { HeroService } from "../hero.service";
 import { HeroDetailComponent } from "./hero-detail.component";
 import { Location } from '@angular/common';
+import { of } from "rxjs";
+import { FormsModule } from "@angular/forms";
 
 
 describe('HeroDetailComponent', () => {
-    let fixture, mockActivatedRoute, mockHeroService, mockLocation;
+    let fixture: ComponentFixture<HeroDetailComponent>;
+    let mockActivatedRoute, mockHeroService, mockLocation;
 
     beforeEach(() => {
         mockActivatedRoute = {
-            snapshot: { paraMap: { get: () => { return '3'; } } }
+            snapshot: { paramMap: { get: () => { return '3'; } } }
         }
         mockHeroService = jasmine.createSpyObj(['getHero', 'updateHero']);
         mockLocation = jasmine.createSpyObj(['back']);
+
         TestBed.configureTestingModule({
+            imports: [FormsModule],
             declarations: [HeroDetailComponent],
             providers: [
-              {provider: ActivatedRoute, useValue: mockActivatedRoute},
-              {provider: HeroService, useValue: mockHeroService},
-              {provider: Location, useValue: mockLocation},
+              {provide: ActivatedRoute, useValue: mockActivatedRoute},
+              {provide: HeroService, useValue: mockHeroService},
+              {provide: Location, useValue: mockLocation},
             ]
         });
         fixture = TestBed.createComponent(HeroDetailComponent);
+        
+        mockHeroService.getHero.and.returnValue(of({id:3, name:'SuperW', strength:100}));
+
+    });
+    
+    //to check hero name is rendered out correctly
+    it('should render hero name in a h2 tag', () => {
+        fixture.detectChanges();
+
+        expect(fixture.nativeElement.querySelector('h2').textContent).toContain('SUPERW');
     })
 })
