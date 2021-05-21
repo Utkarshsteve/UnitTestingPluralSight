546949920b0d65b3e2c457ff8227b65732eea552
Utkarshsteve <utkarshsteve7@gmail.com>
2021-05-21 15:52:41 +0530

    Added Async Code

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -4,6 +4,7 @@ import { Location } from '@angular/common';
 
 import { Hero }         from '../hero';
 import { HeroService }  from '../hero.service';
+import { time } from 'console';
 
 @Component({
   selector: 'app-hero-detail',
@@ -34,7 +35,25 @@ export class HeroDetailComponent implements OnInit {
   }
 
  save(): void {
+   debounce(() => {
     this.heroService.updateHero(this.hero)
       .subscribe(() => this.goBack());
+   }, 250, false)();
+    
   }
 }
+
+  function debounce(func, wait, immediate) {
+      var timeout;
+      return function() {
+        var context = this, args = arguments;
+        var later = function() {
+          timeout = null;
+          if(!immediate) func.apply(context, args);
+        };
+        var callNow = immediate && !timeout;
+        clearTimeout(timeout);
+        timeout = setTimeout(later, wait);
+        if (callNow) func.apply(context, args);
+      };
+  }
